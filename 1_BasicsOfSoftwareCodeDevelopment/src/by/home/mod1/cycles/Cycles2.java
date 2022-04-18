ec6c3b3e34a5f474e72f817b9bca32d593e8a574
caveks <caveks@gmail.com>
2022-04-18 14:14:26 +0300

    Introduction To Java Online

---
+++ 1_BasicsOfSoftwareCodeDevelopment/src/by/home/mod1/cycles/Cycles2.java
@@ -0,0 +1,37 @@
+package by.home.mod1.cycles;
+
+import java.util.Scanner;
+
+//Вычислить значения функции на отрезке [a,b] с шагом h
+
+public class Cycles2 {
+
+	public static void main(String[] args) {
+
+		int a, b, h;
+		int x, y;
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Введите значения отрезка [a,b] и шаг h:");
+		System.out.print("Введите значение переменной a = ");
+		a = s.nextInt();
+		System.out.print("Введите значение переменной b = ");
+		b = s.nextInt();
+		System.out.print("Введите шаг h = ");
+		h = s.nextInt();		
+
+		System.out.println("Значения функции на отрезке [" + a + "," + b + "] с шагом " + h);
+		for(x = a; x <= b; x = x + h)  {
+			if (x > 2) {
+				y = x;
+				System.out.println("y = " + y);
+			} else if (x <= 2){
+				y = -x;
+				System.out.println("y = " + y);
+			}	
+		}
+
+		s.close();
+	}
+
+}
