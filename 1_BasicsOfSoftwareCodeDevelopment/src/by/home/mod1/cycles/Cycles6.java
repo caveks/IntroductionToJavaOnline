ce43d198e7496984b36710530fb96957a0fc57c5
caveks <caveks@gmail.com>
2022-04-21 16:44:25 +0300

    Introduction To Java Online

---
+++ 1_BasicsOfSoftwareCodeDevelopment/src/by/home/mod1/cycles/Cycles6.java
@@ -0,0 +1,16 @@
+package by.home.mod1.cycles;
+
+//Вывести на экран соответствие между символами и их численными обозначениями в памяти компьютера
+
+public class Cycles6 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Соответствие между символами и их численными обозначениями(таблица символов ASCII)");
+		
+		for (int i = 33; i <= 255; i++) {
+			System.out.println((char)i + " -> " + i);
+		}
+	}
+
+}
