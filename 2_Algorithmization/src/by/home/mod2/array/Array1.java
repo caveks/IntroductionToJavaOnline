f990578e9bd45f50b01d24c37b9b02d726610740
caveks <caveks@gmail.com>
2022-07-29 21:07:45 +0300

    Introduction To Java Online

---
+++ 2_Algorithmization/src/by/home/mod2/array/Array1.java
@@ -0,0 +1,23 @@
+package by.home.mod2.array;
+
+//В массив A[N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному K. 
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
