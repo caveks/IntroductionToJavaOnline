1d75ff2f4f5e86b6f5b1d678e066204d3b482c95
caveks <caveks@gmail.com>
2022-12-01 21:16:54 +0100

    Introduction To Java Online

--- 2_Algorithmization/src/by/home/mod2/array/Array1.java
+++ 2_Algorithmization/src/by/home/mod2/array/Array1.java
@@ -6,8 +6,13 @@ public class Array1 {
 
 	public static void main(String[] args) {
 		
-	
+		int[] a = new int[5];
+		int k = 3;
 		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int) (Math.random() * 10 + 1);
+			System.out.println(a[i]);
+		}
 		
 		
 		
