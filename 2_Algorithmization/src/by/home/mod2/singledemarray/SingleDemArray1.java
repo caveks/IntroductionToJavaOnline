5df6e224f65937dd19480cce35e3dcf1361e9bb8
caveks <caveks@gmail.com>
2022-12-06 12:52:23 +0100

    Introduction To Java Online

---
+++ 2_Algorithmization/src/by/home/mod2/singledemarray/SingleDemArray1.java
@@ -0,0 +1,31 @@
+package by.home.mod2.singledemarray;
+
+//В массив A[N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному K. 
+
+public class SingleDemArray1 {
+
+	public static void main(String[] args) {
+		
+		int[] a = new int[5];
+		int k = 3;
+		int s = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int) (Math.random() * 100 + 1);
+		}
+		
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 3 == 0 ){
+				s = s + a[i];
+			}
+		}
+		
+		if (s != 0) {
+			System.out.println("Сумма элементов массива которые кратны данному числу " + k + ": " + s);
+		} else {
+			System.out.println("В массиве нет элементов которые кратны данному числу " + k);
+		}
+			
+	}
+
+}
