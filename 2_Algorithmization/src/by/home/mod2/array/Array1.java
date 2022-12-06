e605c803fe13c7fd642b37eaf681b2061445a2d9
caveks <caveks@gmail.com>
2022-12-06 12:18:03 +0100

    Introduction To Java Online

--- 2_Algorithmization/src/by/home/mod2/array/Array1.java
+++ 2_Algorithmization/src/by/home/mod2/array/Array1.java
@@ -8,21 +8,24 @@ public class Array1 {
 		
 		int[] a = new int[5];
 		int k = 3;
+		int s = 0;
 		
 		for (int i = 0; i < a.length; i++) {
-			a[i] = (int) (Math.random() * 10 + 1);
-			System.out.println(a[i]);
+			a[i] = (int) (Math.random() * 100 + 1);
 		}
 		
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 3 == 0 ){
+				s = s + a[i];
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-
+		if (s != 0) {
+			System.out.println("Сумма элементов массива которые кратны данному числу " + k + ": " + s);
+		} else {
+			System.out.println("В массиве нет элементов которые кратны данному числу " + k);
+		}
+			
 	}
 
 }
