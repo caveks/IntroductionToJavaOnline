3a4dce2a60c67a6f6566813186b6fcdf1a333792
caveks <caveks@gmail.com>
2022-12-06 13:07:28 +0100

    Introduction To Java Online

---
+++ 2_Algorithmization/src/by/home/mod2/singledemarray/SingleDemArray2.java
@@ -0,0 +1,12 @@
+package by.home.mod2.singledemarray;
+
+//Дана последовательность действительных чисел a1, a2, ...an. Заменить все её члены, больщие данного Z, этим чмслом. Подсчитать колличество замен.
+
+public class SingleDemArray2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
